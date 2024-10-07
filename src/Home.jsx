220f249aad1af78d9eb4fdeb819a93e864bbae94
EddyKay86 <eddykay707@gmail.com>
2024-10-07 12:52:29 +0000

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,11 @@
+const Home = ()=>{
+    return(
+        <>
+            <h1>Hello Welcome to React World!!!</h1>
+            <p>React is amazing</p>
+        </>
+        
+    )
+}
+
+export default Home;
